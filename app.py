f1767455845fa5a87e52e365349141ba3451452e
Sini Liu <aisinilandia@gmail.com>
2021-02-03 20:31:17 +0200

    add navbar with bootstrap

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return "Heipparallaa!"
+    return render_template("index.html")
 
 
 
