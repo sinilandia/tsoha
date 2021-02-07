53cea4dd106b1a2229f9c5044db24df27818e008
Sini Liu <aisinilandia@gmail.com>
2021-02-07 19:49:57 +0200

    import werkzeug, update schema.sql

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from datetime import date, datetime, timedelta
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 
--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 CREATE TABLE ravintolat (id SERIAL PRIMARY KEY, nimi TEXT, osoite TEXT, puh TEXT, email TEXT, lounashinta TEXT);
 CREATE TABLE lounaat (id SERIAL PRIMARY KEY, nimi TEXT, pvm DATE, peukut INTEGER, ravintola_id INTEGER REFERENCES ravintolat(id));
+CREATE TABLE kayttajat (id SERIAL PRIMARY KEY, tunnus TEXT, salasana TEXT, admin BOOLEAN, ravintoloitsija INTEGER REFERENCES ravintolat(id));
