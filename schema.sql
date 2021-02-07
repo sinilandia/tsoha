54041db465463c66fcd74759bceade01df59f129
Sini Liu <aisinilandia@gmail.com>
2021-02-07 15:54:24 +0200

    korjaus schema.sql

--- schema.sql
+++ schema.sql
@@ -1,2 +1,2 @@
 CREATE TABLE ravintolat (id SERIAL PRIMARY KEY, nimi TEXT, osoite TEXT, puh TEXT, email TEXT, lounashinta TEXT);
-CREATE TABLE lounashinta (id SERIAL PRIMARY KEY, nimi TEXT, pvm DATE, peukut INTEGER, ravintola_id INTEGER REFERENCES ravintolat(id));
+CREATE TABLE lounaat (id SERIAL PRIMARY KEY, nimi TEXT, pvm DATE, peukut INTEGER, ravintola_id INTEGER REFERENCES ravintolat(id));
