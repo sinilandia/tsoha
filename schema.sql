0aac701c6dd70cb75a058e2ba0de11d8be91f931
Sini Liu <aisinilandia@gmail.com>
2021-02-24 20:26:56 +0200

    update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,3 @@
 CREATE TABLE ravintolat (id SERIAL PRIMARY KEY, nimi TEXT, osoite TEXT, puh TEXT, email TEXT, lounashinta TEXT);
 CREATE TABLE lounaat (id SERIAL PRIMARY KEY, nimi TEXT, pvm DATE, peukut INTEGER, ravintola_id INTEGER REFERENCES ravintolat(id) NOT NULL);
-CREATE TABLE kayttajat (id SERIAL PRIMARY KEY, tunnus TEXT, salasana TEXT, ravintoloitsija INTEGER REFERENCES ravintolat(id));
+CREATE TABLE kayttajat (id SERIAL PRIMARY KEY, tunnus TEXT, salasana TEXT, ravintola_id INTEGER REFERENCES ravintolat(id));
