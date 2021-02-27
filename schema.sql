08da8bb517f79719884e0654538e5dc3b19988cf
Sini Liu <aisinilandia@gmail.com>
2021-02-27 21:48:48 +0200

    add button favorite restaurant

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE TABLE ravintolat (id SERIAL PRIMARY KEY, nimi TEXT, osoite TEXT, puh TEXT, email TEXT, lounashinta TEXT);
 CREATE TABLE lounaat (id SERIAL PRIMARY KEY, nimi TEXT, pvm DATE, peukut INTEGER, ravintola_id INTEGER REFERENCES ravintolat(id) NOT NULL);
 CREATE TABLE kayttajat (id SERIAL PRIMARY KEY, tunnus TEXT, salasana TEXT, ravintola_id INTEGER REFERENCES ravintolat(id));
+CREATE TABLE favorites (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES kayttajat(id), restaurant_id INTEGER REFERENCES ravintolat(id));
